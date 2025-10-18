96dbcbbbc1c790b02ebb1dadd56772f660945c86
YazanAlawneh <yazan.alawneh-e@maf.ae>
2025-10-18 07:31:23 +0300

    pod file update and solve firebase issue

--- ios/Spa/AppDelegate.h
+++ ios/Spa/AppDelegate.h
@@ -1,6 +1,6 @@
-#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
+#import <React/RCTBridgeDelegate.h>
 
-@interface AppDelegate : RCTAppDelegate
-
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+@property (nonatomic, strong) UIWindow *window;
 @end
--- ios/Spa/AppDelegate.mm
+++ ios/Spa/AppDelegate.mm
@@ -8,6 +8,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
 
   self.moduleName = @"Bella";
   // You can add your custom initial props in the dictionary below.
